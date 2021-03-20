ba7e6c9cdfbbb6b1c00e045e74a0fdad84657a31
Tarek Mostafa <tarekmostafa.dev@gmail.com>
2021-03-21 00:30:49 +0200

    clear extra spaces

--- landing page/js/app.js
+++ landing page/js/app.js
@@ -1,28 +1,41 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
  */
 
 /**
  * Define Global Variables
- * 
+ *
  */
+
 const navList = document.querySelector("#navbar__list");
 const fragment = document.createDocumentFragment();
-const entries = document.querySelectorAll("section");
+const sectionElement = document.querySelectorAll("section");
 
-entries.forEach(function(section) {
-    const navLi = document.createElement("li")
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+sectionElement.forEach(function(section) {
+    const navLi = document.createElement("li");
 
     const anchor = document.createElement("a");
     anchor.className = "menu__link";
@@ -32,34 +45,21 @@ entries.forEach(function(section) {
 
     navLi.appendChild(anchor);
     fragment.appendChild(navLi);
-})
+});
+
+
+// build the nav
+
 navList.appendChild(fragment);
 
-function scrollToSection(event) {
-    event.preventDefault();
+// Add class 'active' to section when near top of viewport
 
-    const sectionId = this.getAttribute("href");
-    const sectionElement = document.querySelector(sectionId);
-    sectionElement.scrollIntoView({ block: "center", behavior: "smooth" });
-    // console.log(event);
-}
-/**
- * End Global Variables
- * Start Helper Functions
- * 
- */
 if (!!window.IntersectionObserver) {
-
-    let observer = new IntersectionObserver(function(entries, observer) {
-
-        entries.forEach(function(section) {
-
+    let observer = new IntersectionObserver(function(sectionElement, observer) {
+        sectionElement.forEach(function(section) {
             if (section.isIntersecting) {
-
                 section.target.className = "your-active-class";
 
-
-                //ToDo Add active nav
                 const sectionId = section.target.id;
 
                 // observer.unobserve(section.target);
@@ -67,77 +67,29 @@ if (!!window.IntersectionObserver) {
                 section.target.className = "";
             }
         });
-
     });
 
-
-    document.querySelectorAll('section').forEach(section => { observer.observe(section) });
+    document.querySelectorAll("section").forEach(function(section) {
+        observer.observe(section);
+    });
 }
 
-// else document.querySelector('#warning').style.display = 'block';
-
-
-
-
-
-
-window.addEventListener("scroll", () => {
-
-
-
-    // entries.forEach((sec) => {
-    //     const rect = sec.getBoundingClientRect();
-
-    //     if (rect.top >= -1000 && rect.top <= 1200) {
-    //         entries.forEach((remove) => {
-    //             remove.style.background = "green";
-    //         })
-    //     }
-
-    //     sec.style.background = "gray";
-
-    // })
-});
-
-
-
-
-
-
-// window.addEventListener("scroll", toggleActiveState);
-
-// function toggleActiveState(event) {
-//     entries.forEach(function() {
-//         // entries.className.remove("your-active-class");
-//         // navList.classList.remove()
-//         console.log(event);
-//     })
-// }
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
- */
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
 // Scroll to anchor ID using scrollTO event
-if (!!window.IntersectionObserver) {}
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
  */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
-// Set entries as active
+function scrollToSection(event) {
+    event.preventDefault();
+
+    const sectionId = this.getAttribute("href");
+    const sectionElement = document.querySelector(sectionId);
+    sectionElement.scrollIntoView({ block: "center", behavior: "smooth" });
+}
